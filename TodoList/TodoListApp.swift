20991b33ef7f769a587207ad210482b44d70668c
Ben Haas <ben@benhaas.io>
2024-05-30 10:29:39 -0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ben Haas on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
