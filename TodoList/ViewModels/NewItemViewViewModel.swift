19bf799a0c172df343b3c80dbf83f6d52902798d
Ben Haas <ben@benhaas.io>
2024-07-01 15:19:39 -0700

    save todo to firebase

--- TodoList/ViewModels/NewItemViewViewModel.swift
+++ TodoList/ViewModels/NewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
@@ -14,7 +16,32 @@ class NewItemViewViewModel: ObservableObject {
     
     init() {}
     
-    func save() {}
+    func save() {
+        guard canSave else {
+            return
+        }
+        
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let newId = UUID().uuidString
+        let newItem = TodoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document("uId")
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+    }
     
     var canSave: Bool {
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
